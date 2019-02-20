48aed32582f0f9d66174096d6852c3e8ae9fe4eb
π <jettom@hotmail.com>
2019-02-20 21:33:02 +0900

    2to3 end

--- book_spiderbook/1.4.1.5.py
+++ book_spiderbook/1.4.1.5.py
@@ -6,13 +6,13 @@ import time,os
 
 def proc_send(pipe,urls):
     for url in urls:
-        print "Process(%s) send: %s" %(os.getpid(),url)
+        print("Process(%s) send: %s" %(os.getpid(),url))
         pipe.send(url)
         time.sleep(random.random())
 
 def proc_recv(pipe):
     while True:
-        print "Process(%s) rev:%s" %(os.getpid(),pipe.recv())
+        print("Process(%s) rev:%s" %(os.getpid(),pipe.recv()))
         time.sleep(random.random())
 
 '''
