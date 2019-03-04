3ac0371358ef1bdfdbff4289ca1bfbdf0dc20fe2
π <jettom@hotmail.com>
2019-03-05 05:08:48 +0900

    update

--- stockA-read.py
+++ stockA-read.py
@@ -1,3 +1,4 @@
+# -*-coding:UTF-8-*-
 import pandas as pd
 import csv
 #http://s.askci.com/stock/a/?reportTime=2017-12-31&pageNum=%s1
@@ -6,11 +7,11 @@ import csv
 #fourth table [3]
 
 #http://s.askci.com/stock/a/?reportTime=2017-12-31&pageNum=i
-#再注意一下其他参数：
-#a：表示A股，把a替换为h，表示港股；把a替换为xsb，则表示新三板。那么，在网址分页for循环外部再加一个for循环，就可以爬取这三个股市的股票了。
+# 再注意一下其他参数：
+# a：表示A股，把a替换为h，表示港股；把a替换为xsb，则表示新三板。那么，在网址分页for循环外部再加一个for循环，就可以爬取这三个股市的股票了。
 
-for i in range(1,178):
+for i in range(1, 179):
     url = 'http://s.askci.com/stock/a/?reportTime=2017-12-31&pageNum=%s' % (str(i))
-    tb = pd.read_html(url)[3] #
+    tb = pd.read_html(url)[3]  #
     tb.to_csv(r'1.csv', mode='a', encoding='utf_8_sig', header=1, index=0)
     print('di'+str(i)+'page done')
