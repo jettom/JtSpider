099736600333d3804e43db34ba734ce45e53de87
π <jettom@hotmail.com>
2019-04-05 16:46:03 +0900

    update import funtion comment

--- sample/myplot_test.py
+++ sample/myplot_test.py
@@ -3,3 +3,14 @@ from mymodule import myplot
 # test import from local module
 x = [0.5**i for i in range(20)]
 myplot.myplot(x)
+
+
+'''
+# import function
+from mymodule.myplot import myplot
+
+# test import from local module
+x = [0.5**i for i in range(20)]
+myplot(x)
+
+'''
